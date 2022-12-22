965c92381e6a7f9bac86ee7217ba6bcaf0b51bae
pretzL <ericpretzinger@gmail.com>
2022-12-22 14:19:37 +0100

    Adds functioning Hangman game

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,7 +1,27 @@
-export function HangmanDrawing() {
+const HEAD = <div className="w-[50px] h-[50px] rounded-full border-[10px] border-solid border-black absolute top-[50px] right-[-20px]"></div>;
+
+const BODY = <div className="w-[10px] h-[100px] bg-black absolute top-[120px] right-0"></div>;
+
+const RIGHT_ARM = <div className="w-[100px] h-[10px] bg-black absolute top-[150px] right-[-100px] rotate-[-30deg] origin-bottom-left"></div>;
+
+const LEFT_ARM = <div className="w-[100px] h-[10px] bg-black absolute top-[150px] right-[10px] rotate-[30deg] origin-bottom-right"></div>;
+
+const RIGHT_LEG = <div className="w-[100px] h-[10px] bg-black absolute top-[210px] right-[-90px] rotate-[60deg] origin-bottom-left"></div>;
+
+const LEFT_LEG = <div className="w-[100px] h-[10px] bg-black absolute top-[210px] right-0 rotate-[-60deg] origin-bottom-right"></div>;
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number;
+};
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
   return (
     <div className="relative">
-      <div className="h-[10px] w-[200px] bg-black ms-[120px]" />
+      {BODY_PARTS.slice(0, numberOfGuesses)}
+      <div className="h-[50px] w-[10px] bg-black absolute top-0 right-0" />
+      <div className="h-[10px] w-[220px] bg-black ms-[120px]" />
       <div className="h-[400px] w-[10px] bg-black ms-[120px]" />
       <div className="h-[10px] w-[250px] bg-black" />
     </div>
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,24 @@
-export function HangmanWord() {
-  return;
+type HangmanWordProps = {
+  guessedLetters: string[];
+  wordToGuess: string;
+  reveal?: boolean;
+};
+
+export function HangmanWord({ reveal = false, guessedLetters, wordToGuess }: HangmanWordProps) {
+  return (
+    <div className="flex gap-1 text-3xl bold uppercase">
+      {wordToGuess.split("").map((letter, index) => (
+        <span className="border-b-2 border-black" key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden",
+              color: !guessedLetters.includes(letter) && reveal ? "red" : "black",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
