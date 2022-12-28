6fa69c890d964f77af0609fe117ec3291c20d5d5
pretzL <ericpretzinger@gmail.com>
2022-12-28 12:47:35 +0100

    Fix top bar not being full length

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -21,7 +21,7 @@ export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
     <div className="relative">
       {BODY_PARTS.slice(0, numberOfGuesses)}
       <div className="h-[50px] w-[10px] bg-black absolute top-0 right-0" />
-      <div className="h-[10px] w-[220px] bg-black ms-[120px]" />
+      <div className="h-[10px] bg-black ms-[120px]" />
       <div className="h-[400px] w-[10px] bg-black ms-[120px]" />
       <div className="h-[10px] w-[250px] bg-black" />
     </div>
